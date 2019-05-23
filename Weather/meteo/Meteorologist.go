627d4b080b9a307264c9b6a578c850b27e5a7d00
Dmitry Shkut <shkut@strim-tech.com>
2019-05-23 08:57:58 +0300

    >start your variables with lower case исправлено

--- Weather/meteo/Meteorologist.go
+++ Weather/meteo/Meteorologist.go
@@ -8,13 +8,6 @@ import (
 	"os"
 )
 
-// var response = []byte(`{"coord":{"lon":30.34,"lat":53.91},"weather":[{"id":800,"main":"Clear","description":"ясно","icon":"01d"}],
-// "base":"stations","main":{"temp":18.28,"pressure":1012,"humidity":49,"temp_min":16.11,"temp_max":22},
-// "visibility":10000,"wind":{"speed":5,"deg":70,"gust":8},
-// "clouds":{"all":0},"dt":1558456052,
-// "sys":{"type":1,"id":8938,"message":0.0047,"country":"BY","sunrise":1558403263,"sunset":1558461778},
-// "id":625665,"name":"Mahilyow","cod":200}`)
-
 type Meteorologist struct{}
 
 func (m *Meteorologist) GetWeather(city string) (w WeatherT) {
@@ -26,15 +19,13 @@ func (m *Meteorologist) GetWeather(city string) (w WeatherT) {
 	}
 	defer resp.Body.Close()
 
-	BodyByteSlice, err := ioutil.ReadAll(resp.Body)
+	bodyByteSlice, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("error:", err)
 		os.Exit(2)
 	}
 
-	// fmt.Println(string(BodyByteSlice))
-
-	err = json.Unmarshal(BodyByteSlice, &w)
+	err = json.Unmarshal(bodyByteSlice, &w)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
